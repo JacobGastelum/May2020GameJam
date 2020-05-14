5fcdbf6742f361eaabb542054d921e0c6c6164e5
Steven Tran <thisissteventranwow@gmail.com>
2020-05-14 00:45:37 -0700

    Added Clocks Room

---
+++ ClockRooms/Assets/Scripts/ClockScripts/DigitalClock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DigitalClock : Clock
+{
+    public bool militaryClock;
+    public TextMeshPro text;
+
+    public override void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void animClock()
+    {
+
+        string hourText;
+        string minuteText;
+        if(hour < 10)
+        {
+            hourText = "0" + hour.ToString();
+        }
+        else
+        {
+            hourText = hour.ToString();
+        }
+
+        if (minute < 10)
+        {
+            minuteText = "0" + minute.ToString();
+        }
+        else
+        {
+            minuteText = minute.ToString();
+        }
+
+        text.SetText(hourText + ":" + minuteText);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animClock();
+    }
+}
